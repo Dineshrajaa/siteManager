ce62a00cdf9f991ba0b5827503e9b4e54cc181d6
Dinesh Raja <dinesh.rajaguru@qruize.com>
2017-12-08 16:00:03 +0530

    Included API services

--- src/providers/remote-service/remote-service.ts
+++ src/providers/remote-service/remote-service.ts
@@ -21,4 +21,33 @@ export class RemoteServiceProvider {
     // .map((res: Response) => res.json())
   }
 
+  registerEngineer(engineerData) {
+    /* Service to register an engineer */
+    return this.http.post(this.BASE_URL + 'adduser', engineerData, { observe: 'response' })
+  }
+
+  registerProject(projectData) {
+    /* Service to register a project */
+    return this.http.post(this.BASE_URL + 'addProject', projectData, { observe: 'response' })
+  }
+
+  registerComment(commentData) {
+    /* Service to register an engineer */
+    return this.http.post(this.BASE_URL + 'addComment', commentData, { observe: 'response' })
+  }
+
+  fetchEngineers() {
+    /* Service to get the list of engineers */
+    return this.http.get(this.BASE_URL + 'listAllEngineers', { observe: 'response' })
+  }
+
+  fetchProjects() {
+    /* Service to get the list of projects */
+    return this.http.get(this.BASE_URL + 'listAllProjects', { observe: 'response' })
+  }
+
+  fetchComments(projectID) {
+    /* Service to get the list of comments */
+    return this.http.get(this.BASE_URL + 'getComments/' + projectID, { observe: 'response' })
+  }
 }
